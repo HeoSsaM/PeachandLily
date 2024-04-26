944cd7cb422a77ab16d2859de3a2ca0dc9156af4
허혜정 <blessme.5004@gmail.com>
2024-04-26 12:12:46 +0900

    modified url

--- src/pages/ProductAll.js
+++ src/pages/ProductAll.js
@@ -13,7 +13,8 @@ export const ProductAll = () => {
     const getProducts = async () => {
         try {
             let keyword = query.get("q") || "";
-            let url = `http://localhost:5000/products/?q=${keyword}`;
+            /* let url = `http://localhost:5000/products/?q=${keyword}`; */
+            let url = `https://my-json-server.typicode.com/HeoSsaM/peachnlily/products/${id}`;
             let response = await fetch(url);
             let data = await response.json();
             if (data.length < 1) {
--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -7,7 +7,7 @@ export const ProductDetail = () => {
   const [product, setProduct] = useState(null);
   console.log(product)
   const getProductDetail = async () => {
-    let url = `http://localhost:5000/products/${id}`;
+    let url = `https://my-json-server.typicode.com/HeoSsaM/peachnlily/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
